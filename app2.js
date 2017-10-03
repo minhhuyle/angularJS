9f3102b5ad838d325bbb653bcf998dbd5bc35804
Caroline <clhote@ece.fr>
2017-10-03 17:06:10 +0200

    test

---
+++ app2.js
@@ -0,0 +1,12 @@
+/**
+ * Created by CLH3623 on 03/10/2017.
+ */
+(function() {
+
+    angular.module("myApp", [])
+        .controller("editController", [function () {
+
+            alert("hello");
+
+        }])
+}());
