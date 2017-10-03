a04bbabe8cc5113813caae3c17ecf82bfb2029d2
Caroline <clhote@ece.fr>
2017-10-03 14:51:58 +0200

    start qcm

--- app.js
+++ app.js
@@ -8,9 +8,69 @@
 
 
 			self.data = [
-				{title: "HMTL5"},
-				{title: "JS"},
-				{title: "Angular"}
+				{title: "HMTL5",
+                    questions: [{
+                        enonce: "ABHHBHBB",
+                        reponses:["A", "B", "C", "D"],
+                        good: "A"
+                    },
+                        {
+                            enonce: "ABHHBHBB",
+                            reponses:["A", "B", "C", "D"],
+                            good: "B"
+                        }],
+                    questions: [{
+                        enonce: "Ckdoskdlskdd",
+                        reponses:["A", "B", "C", "D"],
+                        good: "C"
+                    },
+                        {
+                            enonce: "iijijijij",
+                            reponses:["A", "B", "C", "D"],
+                            good: "A"
+                        }]},
+				{title: "JS",
+                    questions: [{
+                        enonce: "ABHHBHBB",
+                        reponses:["A", "B", "C", "D"],
+                        good: "A"
+                    },
+                        {
+                            enonce: "ABHHBHBB",
+                            reponses:["A", "B", "C", "D"],
+                            good: "B"
+                        }],
+                    questions: [{
+                        enonce: "Ckdoskdlskdd",
+                        reponses:["A", "B", "C", "D"],
+                        good: "C"
+                    },
+                        {
+                            enonce: "iijijijij",
+                            reponses:["A", "B", "C", "D"],
+                            good: "A"
+                        }]},
+				{title: "Angular",
+                    questions: [{
+                        enonce: "ABHHBHBB",
+                        reponses:["A", "B", "C", "D"],
+                        good: "A"
+                    },
+                        {
+                            enonce: "ABHHBHBB",
+                            reponses:["A", "B", "C", "D"],
+                            good: "B"
+                        }],
+                    questions: [{
+                        enonce: "Ckdoskdlskdd",
+                        reponses:["A", "B", "C", "D"],
+                        good: "C"
+                    },
+                        {
+                            enonce: "iijijijij",
+                            reponses:["A", "B", "C", "D"],
+                            good: "A"
+                        }]}
 			];
 
 
@@ -18,10 +78,34 @@
 
 			self.selectNote = function(data){
 				self.selectData = data;
-			}
+				self.currentQuestion = 0;
+				self.score = 0;
+			};
 
             self.shouldShowSelectedData = function(){
-                return self.selectData != null;
-            }
+            	if(self.selectData != null) {
+                    if(self.currentQuestion >= self.selectData.questions.length) {
+                        return null;
+                    }
+                    return self.selectData != null;
+                }
+            };
+
+            self.showQuestion = function(){
+            	return self.selectData.questions[self.currentQuestion].enonce;
+			};
+
+			self.getReponses = function(){
+            	return self.selectData.questions[self.currentQuestion].reponses;
+			}
+
+			self.changeCurrentQuestion = function(reponse){
+				if(reponse == self.selectData.questions[self.currentQuestion].good) {
+					self.score ++;
+				}
+				self.currentQuestion ++;
+			}
+
+
 		}])
 }());
