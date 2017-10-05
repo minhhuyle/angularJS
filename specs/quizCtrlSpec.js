e48cc4d55e92a0a9dca3c7e0cb38d631c558731d
Minh-Huy LE <minhhuylecontact@gmail.com>
2017-10-05 16:53:17 +0200

    add config

--- specs/quizCtrlSpec.js
+++ specs/quizCtrlSpec.js
@@ -1,9 +1,13 @@
-/**
- * Created by minhhuyle on 05/10/2017.
- */
-describe('quizCtrl', function() {
-    beforeEach(function() {
+describe('Controller: ListCtrl', function() {
+    // Instantiate a new version of my module before each test
+    beforeEach(module('qcmMinh'));
+    var ctrl;
+    // Before each unit test, instantiate a new instance of the controller
+    beforeEach(inject(function($controller) {
+        //ctrl = $controller('quizCtrl');
+    }));
+    it('should have items available on load', function() {
+        //expect(ctrl.type).toEqual("ALL");
     });
-    afterEach(function() {
-    });
+
 });
