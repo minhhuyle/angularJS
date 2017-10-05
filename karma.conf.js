8be90bc66b755e548673ea32aaeb3b4a51455cef
Caroline <clhote@ece.fr>
2017-10-05 15:58:14 +0200

    tests

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(config) {
+    config.set({
+        // base path that will be used to resolve files and exclude
+        basePath: '',
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
+        // list of files / patterns to load in the browser
+        files: [
+            'angular.js',
+            'angular-mocks.js',
+            '/specs/quizCtrlSpec.js',
+        ],
+        // list of files / patterns to exclude
+        exclude: [],
+        // web server port
+        port: 8080,
+        // level of logging possible values: LOG_DISABLE || LOG_ERROR ||LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+    // Start these browsers, currently available:
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+        // Continuous Integration mode
+        // if true, it captures browsers, runs tests, and exits
+        singleRun: false
+})};
