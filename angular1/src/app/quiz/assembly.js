95f686e0ba7ec4fea4edbf78bf092a87af0a926d
Minh-Huy LE <minhhuylecontact@gmail.com>
2017-10-23 23:29:32 +0200

    -refactor editCtrl ngular 1

--- angular1/src/app/quiz/assembly.js
+++ angular1/src/app/quiz/assembly.js
@@ -29,7 +29,7 @@
                         templateUrl: 'app/quiz/quiz.html',
                     })
                     .when('/edit', {
-                        templateUrl: 'app/quiz/edit.html',
+                        templateUrl: 'app/quiz/edit/edit.html',
                     })
                     .otherwise({redirectTo: '/'});
             }])
@@ -251,104 +251,6 @@
                     }
 
                 }])
-
-            .controller("editCtrl", ['qcmListService', '$rootScope', function (qcmListService) {
-
-                var self = this;
-
-                self.numberOfQuestion = 0;
-
-                self.showInputQCM = false;
-
-                self.addQCM = function () {
-                    self.showInputQCM = true;
-                };
-
-                self.shouldShowInputQCM = function () {
-                    return self.showInputQCM;
-                };
-
-                self.submitQcm = function () {
-                    self.showInputQCM = false;
-
-                    var newQcm = {
-                        title: self.qcmTitle,
-                        done: false,
-                        replay: false,
-                        answersUser: [],
-                        indexQuestion: 0,
-                        score: 0,
-                        questions: []
-                    };
-                    qcmListService.addNewQcm(newQcm);
-
-
-                    self.qcmTitle = "";
-                };
-
-                self.getAllQcms = function () {
-                    return qcmListService.getAllQcms();
-                };
-
-                self.deleteQcm = function (qcmToDelete) {
-                    var index = qcmListService.getAllQcms().indexOf(qcmToDelete);
-                    qcmListService.getAllQcms().splice(index, 1);
-                }
-
-                self.selectQcm = function (qcm) {
-                    self.selectedQcm = qcm;
-                    self.selectedQcmIndex = 0;
-                };
-
-
-                self.getSelectedQcm = function () {
-                    return self.selectedQcm;
-                };
-
-                self.getCurrentQuestionOfQcmSelected = function () {
-                    return self.selectedQcm.questions[self.selectedQcmIndex];
-                };
-
-                self.isFirstQuestion = function () {
-                    return self.selectedQcmIndex == 0;
-                };
-
-                self.isLastQuestion = function () {
-                    return (self.selectedQcmIndex >= self.selectedQcm.questions.length - 1);
-                };
-
-                self.showNextQuestion = function () {
-                    self.selectedQcmIndex++;
-                };
-
-                self.showPrevQuestion = function () {
-                    self.selectedQcmIndex--;
-                };
-
-                self.addNewResponse = function () {
-                    var responses = self.selectedQcm.questions[self.selectedQcmIndex].responses;
-                    responses.push("Réponse " + (responses.length + 1));
-                };
-
-                self.addNewQuestion = function () {
-                    var newQuestion = {
-                        enonce: "Enonce",
-                        responses: ["Réponses 1", "Réponses 2"],
-                        good: 0
-                    };
-                    self.selectedQcm.questions.push(newQuestion)
-                };
-
-                self.disableAddResponseWhenItReach4 = function () {
-                    return self.selectedQcm.questions.length > 0 &&
-                        self.selectedQcm.questions[self.selectedQcmIndex].responses.length >= 4;
-                }
-
-                self.disableEditionWhenNoQuestionPresent = function () {
-                    return self.selectedQcm.questions <= 0;
-                }
-            }
-            ])
         ;
     }
     ()
